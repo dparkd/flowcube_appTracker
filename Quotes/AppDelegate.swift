01f5d61061df5c41599a9402986363ae185034b1
Daniel Park <d.hanul.park@gmail.com>
2017-03-30 15:55:32 -0400

    Initial Commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by Daniel Park on 2017-03-30.
+//  Copyright © 2017 Daniel Park. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
